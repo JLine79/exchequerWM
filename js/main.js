99ef7b4e761efa8d01e066457e6e5a8a36071b34
Jennifer Line <51475586+JLine79@users.noreply.github.com>
2021-11-01 20:51:13 +0000

    slider banner progressed

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* Banner slides */
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("banner-box");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block"
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
