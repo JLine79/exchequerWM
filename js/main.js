b5a8e958de3132ad5c7fba5c6d767495bb0ff9fb
Jennifer Line <51475586+JLine79@users.noreply.github.com>
2021-11-03 09:31:37 +0000

    working on flipcards and positioning

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* Banner slides */
 
-let slideIndex = 0;
+/*let slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -12,7 +12,7 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block"
   setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+}*/
 
 /* FAQ Accordion */
 
