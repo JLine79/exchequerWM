6383458537e8ddce8189b1e376b47c441cefe00c
Jennifer Line <51475586+JLine79@users.noreply.github.com>
2021-11-04 01:01:39 +0000

    not perfect but ready to send

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+/* Burger Menu */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("showNav");
+    navMenu.classList.toggle("showNav");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("showNav");
+    navMenu.classList.remove("showNav");
+}
+
 /* Banner slides */
 
 let slideIndex = 0;
